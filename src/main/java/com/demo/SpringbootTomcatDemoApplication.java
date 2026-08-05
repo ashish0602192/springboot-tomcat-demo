9b00d9080c10d07332c48eae05e2da14f6b4bf86
Ashish <cloudtechakp@gmail.com>
2026-08-05 14:06:59 +0400

    Initial Spring Boot Tomcat application

---
+++ src/main/java/com/demo/SpringbootTomcatDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTomcatDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTomcatDemoApplication.class, args);
+    }
+
+}
